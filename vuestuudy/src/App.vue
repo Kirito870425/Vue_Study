e6ae18c41666dc6d36197e97be780cd742a9db3f
Kirito870425 <dsa50163@gmail.com>
2023-11-08 00:30:01 +0800

    init:firstCommit

---
+++ vuestuudy/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+import TodoList from "@/components/TodoList.vue";
+// const todos = ref(["完成作業", "購物清單", "運動"]);
+export default defineComponent({
+  setup() {
+    const todos = ref([] as string[]);
+
+    return {
+      todos,
+    };
+  },
+});
+</script>
---
+++ vuestuudy/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h2>代辦清單</h2>
+      <input v-model="newTodo" @keyup.enter="addTodo" placeholder="新增代辦事項" />
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+    props: {
+      todos: {
+        type: Array as PropType<string[]>,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        newTodo: '',
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo) {
+          this.todos.push(this.newTodo);
+          this.newTodo = '';
+        }
+      },
+    },
+  });
+  </script>
+  
